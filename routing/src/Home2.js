1f8905a448399064289f5655754a240beafc6b70
Rajendra Arora <rajendra.arora@treez.io>
2023-05-02 08:57:06 +0530

    added routing

---
+++ routing/src/Home2.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Home2() {
+  return (
+    <div>This is Home2 page</div>
+  )
+}
---
+++ routing/src/Home3.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Home3() {
+  return (
+    <div>This is Home3 page</div>
+  )
+}
