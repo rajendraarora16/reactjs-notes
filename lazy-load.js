63ed90d72ffe675d489bbc5dbccd724211958fd2
Rajendra arora <contact.rajendraarora@gmail.com>
2024-08-06 08:07:12 +0530

    lazy load changes

---
+++ lazy-load.js
@@ -0,0 +1,9 @@
+const LazyComponent = React.lazy(() => import('./LazyComponent'));
+
+function App() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <LazyComponent />
+    </Suspense>
+  );
+}
