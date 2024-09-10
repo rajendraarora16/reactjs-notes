9a1fc49548e8477a2ff9b1adc7eb85dc46f9e48b
Rajendra arora <contact.rajendraarora@gmail.com>
2024-09-10 13:18:22 +0530

    Update store.js

--- redux-toolkit-simple/store.js
+++ redux-toolkit-simple/store.js
@@ -1,8 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterSlice from "./counterSlice";
 
+// Custom middleware example
+const loggerMiddleware = (storeAPI) => (next) => (action) => {
+  console.log('Dispatching action:', action);
+  let result = next(action); // Pass the action to the next middleware or reducer
+  console.log('Next state:', storeAPI.getState());
+  return result;
+};
+
 const store = configureStore({
     reducer: counterSlice,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(loggerMiddleware),
 });
 
 export default store;
